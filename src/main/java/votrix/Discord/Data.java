44d17bd90d6512b3e63ef0f65e7577f29494eb77
ExZiByte <exzibyte@gmail.com>
2019-08-02 20:48:43 -0500

    Die

--- src/main/java/votrix/Discord/Data.java
+++ src/main/java/votrix/Discord/Data.java
@@ -1,7 +1,16 @@
 package votrix.Discord;
 
+import java.util.Random;
+
 public class Data {
 
     public final String prefix = "~";
 
+    public static int getColor() {
+
+        Random obj = new Random();
+        int rand_num = obj.nextInt(0xffffff + 1);
+
+        return rand_num;
+    }
 }
---
+++ src/main/java/votrix/Discord/commands/Tempmute.java
@@ -0,0 +1,44 @@
+package votrix.Discord.commands;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import votrix.Discord.Data;
+import votrix.Discord.utils.RoleCheck;
+
+import java.awt.*;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+public class Tempmute extends ListenerAdapter {
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        String[] args = event.getMessage().getContentRaw().split("\\s+");
+        Data data = new Data();
+        RoleCheck rc = new RoleCheck();
+        EmbedBuilder eb = new EmbedBuilder();
+        EmbedBuilder muted = new EmbedBuilder();
+        Role muteRole;
+        if(args[0].equalsIgnoreCase(data.prefix + "tempmute")){
+            event.getMessage().delete().queue();
+            if(rc.isOwner(event) || rc.isDeveloper(event) || rc.isAdministrator(event) || rc.isModerator(event)){
+                if(args[1].equalsIgnoreCase("help")){
+                    eb.setDescription("Tempmute Help\n\n```\n~tempmute @member <length><length multiplier> [reason]\n```");
+                    eb.setColor(new Color(data.getColor()));
+                    eb.setTimestamp(Instant.now());
+                    eb.setFooter("Votrix Tempmute Help", event.getJDA().getSelfUser().getEffectiveAvatarUrl());
+
+                    event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                       eb.clear();
+                       message.delete().queueAfter(10, TimeUnit.SECONDS);
+                    });
+                }
+                else if(args.length < 3){
+                    eb.setDescription("You didn't specify enough arguments");
+                }
+            }
+        }
+    }
+
+}

27e6b8746d71afc487d54ce482d9129b3630e46e
ExZiByte <exzibyte@gmail.com>
2019-08-09 14:34:48 -0500

    Fixed a memory leak ooof

--- src/main/java/votrix/Discord/commands/Moderation/Tempmute.java
+++ src/main/java/votrix/Discord/commands/Moderation/Tempmute.java
@@ -261,6 +261,7 @@ public class Tempmute extends ListenerAdapter {
                                     });
                                 });
                             });
+                            reason = "";
                         }
                     }
                 }

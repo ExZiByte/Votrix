2c40717c297349d3888b7efcf9a97d9c07d994b6
ExZiByte <exzibyte@gmail.com>
2019-09-22 12:00:42 -0500

    Testing restart script

--- src/main/java/votrix/Discord/listeners/Ready.java
+++ src/main/java/votrix/Discord/listeners/Ready.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -18,33 +19,30 @@ public class Ready extends ListenerAdapter {
     public void onReady(ReadyEvent event){
         Data data = new Data();
         EmbedBuilder eb = new EmbedBuilder();
+        event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
         Game[] games = { Game.playing("with Somato Setchup"), Game.playing("with Biscuits"), Game.watching("Toe Tucks"), Game.watching("https://twitch.tv/kylaak_") };
         ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
-        ses.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                Random random = new Random();
-                int game = random.nextInt(games.length);
-                event.getJDA().getPresence().setGame(games[game]);
-            }
+        ses.scheduleAtFixedRate(() -> {
+            Random random = new Random();
+            int game = random.nextInt(games.length);
+            event.getJDA().getPresence().setGame(games[game]);
         }, 0, 2, TimeUnit.MINUTES);
 
         eb.setDescription("Restarting... Be back up in 30 - 45 seconds");
         eb.setColor(new Color(data.getColor()));
         eb.setTimestamp(Instant.now());
         eb.setFooter("Bot Restart", data.getSelfAvatar(event));
+
         new java.util.Timer().schedule(
             new java.util.TimerTask() {
                 @Override
                 public void run() {
                     data.getLogChannel(event).sendMessage(eb.build()).queue((message) -> {
-                        event.getJDA().getGuildById("578937882023034901").getTextChannelsByName("general", true).get(0).sendMessage(eb.build()).queue((message1) -> {
                             event.getJDA().shutdownNow();
                             System.exit(1);
-                        });
                     });
                 }
-            },  30000
+            },  60000
         );
     }
 

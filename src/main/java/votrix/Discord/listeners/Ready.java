ad99b28fb3ff4ea426478c325b5504cce5cf80e0
ExZiByte <exzibyte@gmail.com>
2019-09-21 21:43:26 -0500

    Added auto shutdown and restart after 12 hours

--- src/main/java/votrix/Discord/listeners/Ready.java
+++ src/main/java/votrix/Discord/listeners/Ready.java
@@ -1,10 +1,13 @@
 package votrix.Discord.listeners;
 
+import java.awt.*;
+import java.time.Instant;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -14,8 +17,8 @@ public class Ready extends ListenerAdapter {
 
     public void onReady(ReadyEvent event){
         Data data = new Data();
-        //Game[] games = { Game.playing("with Somato Setchup"), Game.playing("with Biscuits"), Game.watching("Toe Tucks"), Game.watching("https://twitch.tv/kylaak_") };
-        Game[] games = {Game.playing("on the developmentally delayed track")};
+        EmbedBuilder eb = new EmbedBuilder();
+        Game[] games = { Game.playing("with Somato Setchup"), Game.playing("with Biscuits"), Game.watching("Toe Tucks"), Game.watching("https://twitch.tv/kylaak_") };
         ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
         ses.scheduleAtFixedRate(new Runnable() {
             @Override
@@ -25,6 +28,24 @@ public class Ready extends ListenerAdapter {
                 event.getJDA().getPresence().setGame(games[game]);
             }
         }, 0, 2, TimeUnit.MINUTES);
+
+        eb.setDescription("Restarting... Be back up in 30 - 45 seconds");
+        eb.setColor(new Color(data.getColor()));
+        eb.setTimestamp(Instant.now());
+        eb.setFooter("Bot Restart", data.getSelfAvatar(event));
+        new java.util.Timer().schedule(
+            new java.util.TimerTask() {
+                @Override
+                public void run() {
+                    data.getLogChannel(event).sendMessage(eb.build()).queue((message) -> {
+                        event.getJDA().getGuildById("578937882023034901").getTextChannelsByName("general", true).get(0).sendMessage(eb.build()).queue((message1) -> {
+                            event.getJDA().shutdownNow();
+                            System.exit(1);
+                        });
+                    });
+                }
+            },  180000
+        );
     }
 
 }
--- src/main/java/votrix/Discord/utils/Data.java
+++ src/main/java/votrix/Discord/utils/Data.java
@@ -3,6 +3,7 @@ package votrix.Discord.utils;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import org.bson.BSON;
 import org.bson.Document;
@@ -19,6 +20,11 @@ public class Data {
     public static TextChannel getLogChannel(GuildMessageReceivedEvent event) {
         return event.getGuild().getTextChannelById("598948078741094400");
     }
+
+    public static TextChannel getLogChannel(ReadyEvent event) {
+        return event.getJDA().getGuildById("578937882023034901").getTextChannelById("598948078741094400");
+    }
+
 
     public static String getPrefix() {
         String prefix;
@@ -52,6 +58,10 @@ public class Data {
     }
 
     public static String getSelfAvatar(GuildMessageReceivedEvent event) {
+        return event.getJDA().getSelfUser().getEffectiveAvatarUrl();
+    }
+
+    public static String getSelfAvatar(ReadyEvent event) {
         return event.getJDA().getSelfUser().getEffectiveAvatarUrl();
     }
 
